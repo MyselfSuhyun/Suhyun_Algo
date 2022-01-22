7c649644af83c955eea5d7672bfb32ade647dce3
suhyun <rnfmal0301@naver.com>
2022-01-22 19:48:37 +0900

    220122 commit

--- Backjoon/0000_taratest.py
+++ Backjoon/0000_taratest.py
@@ -1,9 +1,6 @@
-a = [1, 2, 3, 4]
-b = [11, 22, 33, 44, 55, 66, 77, 88]
-c = [0] * len(b)
-print(c)
-for i in range(len(c)):
-    c[i] = a[i%len(a)]
+from itertools import permutations
 
-print(c)
+arr = list(map(str,[1,2,3,4,5]))
 
+arr = ", ".join(arr)
+print(f'<{arr}>')
